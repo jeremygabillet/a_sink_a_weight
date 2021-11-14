b6ebe2449b018f17767c4f932954b59f1e347873
jeremygabillet <jeremy.gabillet@gmail.com>
2021-11-14 18:27:32 +0100

    Finishing quest

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace a_sink_a_weight
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int rnd1 = random.Next(1000, 5000);
+            int rnd2 = random.Next(1000, 5000);
+            
+            await Task.Delay(rnd1);
+            await Task.Delay(rnd2);
+            Console.WriteLine($"Task rnd1 executed in {rnd1} milliseconds.");
+            Console.WriteLine($"Task rnd2 executed in {rnd2} milliseconds.");
+            return 0;
         }
     }
 }
