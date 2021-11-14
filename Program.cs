c8ecf5b848ed4daf8e8e05a9deee967f8942f0ea
jeremygabillet <jeremy.gabillet@gmail.com>
2021-11-14 17:36:25 +0100

    Creating project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace a_sink_a_weight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
